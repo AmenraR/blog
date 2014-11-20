ef91211990eb3c4afb31a1a0299e313a247b53fd
Ezra Alarid <eja.117@gmail.com>
2014-11-20 14:57:35 -0800

    milestone S9 inserting an if statement to check if there was an error connecting to the connection errors

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,3 +4,11 @@
 
     $connection = new mysqli($host, $username, $password);   //creating the object mysqli that is used to connect to the database
     
+    if($connection->connect_error){     //checks if there was a connection error
+        die("Error: ".$connection->connect_error);
+    }
+    else {
+        echo "Success.".$connection->host_info;
+    }
+    
+    $connection->close();
