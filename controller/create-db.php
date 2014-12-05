92958beeb310ebab1dda17b3da8ed5ba79c45275
Ezra Alarid <eja.117@gmail.com>
2014-12-05 00:32:02 -0800

    milestone S14-15 table is created in the database

--- controller/create-db.php
+++ controller/create-db.php
@@ -14,8 +14,27 @@
         $query = $connection->query("CREATE DATABASE $database");    //send questions to create database
         
         if($query){
-            echo 'Successfully created database: '. $database;
+            echo '<p>Successfully created database: '. $database."</p>";
         }
     }
     
+    else{   //if database already exists, then do this:
+        echo "<p>Database already exists.</p>";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            ."title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
+    
+    if($query){
+        echo "Successfully created table: posts";
+    }
+    else{
+        echo "<p>$connection->error</p>";
+    }
+    
     $connection->close();       //closes connection
+    
+    //watch blog vid 14 3:37
